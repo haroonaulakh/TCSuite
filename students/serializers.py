918d42ca9fecf51394ff6ef72bc0f0752d4c758a
Maham-j <mahamjamil596@gmail.com>
2026-06-04 14:21:24 +0500

    serializer fix

--- students/serializers.py
+++ students/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth.hashers import make_password
 from .models import StudentProfile
 
-
+# serialize student list information 
 class StudentListSerializer(serializers.ModelSerializer):
     class Meta:
         model = StudentProfile
